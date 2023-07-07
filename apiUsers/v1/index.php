d7c2838a5baf48703f98b4d96293aed979376e56
Juan <juan.paez@api-ux.com>
2023-07-07 16:12:41 -0500

    Nueva funcionalidad

--- apiUsers/v1/index.php
+++ apiUsers/v1/index.php
@@ -1392,13 +1392,11 @@ Flight::route('POST /validateLogOut/', function () {
 });
 
 
-Flight::route('POST /putUsersRolBySuperAdmin/', function () {
+Flight::route('POST /putUsersRolBySuperAdmin/@apk/@xapk', function ($apk,$xapk) {
+  
     header("Access-Control-Allow-Origin: *");
-    // Leer los encabezados
-    $headers = getallheaders();
-    
     // Verificar si los encabezados 'Api-Key' y 'Secret-Key' existen
-    if (isset($headers['Api-Key']) && isset($headers['x-api-Key'])) {
+    if (!empty($apk) && !empty($xapk)) {    
         // Leer los datos de la solicitud
         
             
@@ -1406,26 +1404,13 @@ Flight::route('POST /putUsersRolBySuperAdmin/', function () {
             $putId= Flight::request()->data->putId;
        
 
-
-
-
-
-
-        // Acceder a los encabezados
-        $apiKey = $headers['Api-Key'];
-        $xApiKey = $headers['x-api-Key'];
-        
-
-
-
-
         $sub_domaincon=new model_domain();
         $sub_domain=$sub_domaincon->dom();
         $url = $sub_domain.'/crystalCore/apiAuth/v1/authApiKey/';
       
         $data = array(
-          'apiKey' =>$apiKey, 
-          'xApiKey' => $xApiKey
+          'apiKey' =>$apk, 
+          'xApiKey' => $xapk
           
           );
       $curl = curl_init();
