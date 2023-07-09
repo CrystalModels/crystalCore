182e239ab3354ed6401f4e8ec0042bc3f901b292
Juan <juan.paez@api-ux.com>
2023-07-08 19:07:12 -0500

    Nueva funcionalidad

--- apiUsers/v1/index.php
+++ apiUsers/v1/index.php
@@ -2720,7 +2720,7 @@ Flight::route('GET /getProfileInfoLog/@userName/@sessionId', function ($userName
             $conectar=conn();
             
           
-            $query= mysqli_query($conectar,"SELECT u.userId,u.personalMail,u.companyMail,u.internalMail,u.userName,u.sessionCounter,p.profileId,p.name,p.lastName,p.imageUrl,p.totalHours,r.name as rol,t.ranCode,u.isActive,u.status FROM generalUsers u JOIN generalProfiles p ON p.profileId=u.profileId JOIN roles r ON r.rolId=p.rolId JOIN apiTokens t ON t.userId=u.userId JOIN sessionlIST sl ON sl.userName=u.userName where u.userName='$userName' or u.companyMail='$userName' or u.internalMail='$userName' and sl.sessionId='$sessionId'");
+            $query= mysqli_query($conectar,"SELECT u.userId,u.personalMail,u.companyMail,u.internalMail,u.userName,u.sessionCounter,p.profileId,p.name,p.lastName,p.imageUrl,p.totalHours,r.name as rol,t.ranCode,u.isActive,u.status FROM generalUsers u JOIN generalProfiles p ON p.profileId=u.profileId JOIN roles r ON r.rolId=p.rolId JOIN apiTokens t ON t.userId=u.userId JOIN sessionlIST sl ON sl.userName=u.userName where u.userName='$userName' and sl.sessionId='$sessionId'");
                
           
                 $values=[];
